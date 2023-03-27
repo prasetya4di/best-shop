7b3f0ee1ac55f0b0894533307a39f0b58c899472
Pras <prasetyaarfanto@gmail.com>
2023-03-27 13:30:57 +0700

    Add shopping cart repository

---
+++ lib/repository/impl/shopping_cart_repository_impl.dart
@@ -0,0 +1,30 @@
+import 'package:my_marketplace/data/model/entity/shopping_cart.dart';
+import 'package:my_marketplace/data/model/entity/user.dart';
+import 'package:my_marketplace/data/source/local/shopping_cart_dao.dart';
+import 'package:my_marketplace/repository/shopping_cart_repository.dart';
+
+class ShoppingCartRepositoryImpl implements ShoppingCartRepository {
+  final ShoppingCartDao _shoppingCartDao;
+
+  ShoppingCartRepositoryImpl(this._shoppingCartDao);
+
+  @override
+  delete(int id) {
+    _shoppingCartDao.delete(id);
+  }
+
+  @override
+  List<ShoppingCart> get(User user) {
+    return _shoppingCartDao.get(user);
+  }
+
+  @override
+  insert(ShoppingCart shoppingCart) {
+    return _shoppingCartDao.insert(shoppingCart);
+  }
+
+  @override
+  update(ShoppingCart shoppingCart) {
+    return _shoppingCartDao.update(shoppingCart);
+  }
+}
