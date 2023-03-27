2010191cfdb8ef1de4a7ae46e844dc80b247226d
Pras <prasetyaarfanto@gmail.com>
2023-03-27 11:41:51 +0700

    Add user entity

---
+++ lib/data/model/entity/user.dart
@@ -0,0 +1,20 @@
+import 'package:equatable/equatable.dart';
+import 'package:objectbox/objectbox.dart';
+
+@Entity()
+class User extends Equatable {
+  @Id()
+  final int obxId;
+  final String email;
+  final String name;
+  final String address;
+  final DateTime birthDate;
+  final String _password;
+
+  const User(
+      this.email, this.name, this.address, this.birthDate, this._password,
+      {this.obxId = 0});
+
+  @override
+  List<Object?> get props => [email, name, address, birthDate, _password];
+}
