839296555e2bca67666ae896fa76683524740831
Pras <prasetyaarfanto@gmail.com>
2023-03-27 12:50:07 +0700

    Add category dao

---
+++ lib/data/source/local/impl/category_dao_impl.dart
@@ -0,0 +1,24 @@
+import 'package:my_marketplace/data/model/entity/category.dart';
+import 'package:my_marketplace/data/source/local/category_dao.dart';
+import 'package:my_marketplace/objectbox.g.dart';
+
+class CategoryDaoImpl implements CategoryDao {
+  final Box<Category> _box;
+
+  CategoryDaoImpl(this._box);
+
+  @override
+  List<Category> get() {
+    return _box.getAll();
+  }
+
+  @override
+  insert(Category category) {
+    _box.put(category);
+  }
+
+  @override
+  inserts(List<Category> categories) {
+    _box.putMany(categories);
+  }
+}
